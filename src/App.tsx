834711e28a32bcd73cc1cfd17db6741467f88d68
unknown <siddharthmehra24@yahoo.com>
2023-10-08 08:26:08 +0530

    revised

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import Person from './components/Person';
-// import Person from './components/User';
+import StopWatch from './components/StopWatch';
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+  let countRef = useRef<number>(0);
+  let inputRef = useRef<any>();
+  // only call on page load
+  useEffect(() => {
+    // body of hook
+  }, [])
+
+  function App() {
+    const [count , setCount] = useState<number>(0);
+    let countRef = useRef<number>(0)
+    let inputRef = useRef<any>();
+
+    
+  
+
+  // only call on change on count
+  useEffect(() => {
+    // body of hook
+  }, [count])
+  
+
+  function handleClick() {
+     setCount(count + 1);
+  }
+
+  
+  function handleRefClick() {
+    countRef.current += 1;
+    console.log('countRef value', countRef.current);
+    inputRef.current.focus();
+    console.log('inputRef value', inputRef.current);
+  }
+
+   
+
+  console.log('reload the component count value', count, countRef.current);
+
   return (
     <div className="App">
-     <Person/>
+      <input type='text' ref={inputRef}/>
+      <button onClick={handleClick}>{'Count ' + count}</button>
+      <button onClick={handleRefClick}>{'RefCount ' + countRef.current}</button>
+      <hr/>
+      <StopWatch/>
     </div>
   );
+
+  }
+
 }
 
- 
-
 export default App;
-
- 
